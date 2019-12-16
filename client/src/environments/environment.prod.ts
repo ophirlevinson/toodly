72e34541550ae6ecb67245ad04057f909999c66c
Ophir Levinson <levinsonophir@gmail.com>
2019-12-16 21:15:09 +0200

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   serverURL: "https://us-central1-toodlyco.cloudfunctions.net/api",
   firebase: {
-    apiKey: "AIzaSyBRR2eqsaCIuEkIcDQkJca1iKva_Y8jrec",
-    authDomain: "toodlyco.firebaseapp.com",
-    databaseURL: "https://toodlyco.firebaseio.com",
-    projectId: "toodlyco",
-    storageBucket: "toodlyco.appspot.com",
-    messagingSenderId: "176360449015",
-    appId: "1:176360449015:web:98c1c332d2263ce6f784f2"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
